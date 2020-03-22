08a204a7ab0dc469720efcb37f1d4a5d807ce3e2
Jeremy Finn <jeremy.finn@gmail.com>
2020-03-21 22:36:11 -0400

    Add web worker.

--- ec2.tf
+++ ec2.tf
@@ -26,12 +26,22 @@ data "aws_ami" "amzn2" {
 
 }
 
-resource "aws_instance" "app_worker_01" {
+/*resource "aws_instance" "app_worker_01" {
   ami                    = data.aws_ami.amzn2.id
   instance_type          = var.ec2_instance_type
   availability_zone      = "${var.region}a"
   subnet_id              = var.app_subnet_a_id
   tags = {
     Name = "${var.stack_name}-app-worker-01"
+  }
+}*/
+
+resource "aws_instance" "web_worker_01" {
+  ami                    = data.aws_ami.amzn2.id
+  instance_type          = var.ec2_instance_type
+  availability_zone      = "${var.region}a"
+  subnet_id              = var.web_subnet_a_id
+  tags = {
+    Name = "${var.stack_name}-web-worker-01"
   }
 }
